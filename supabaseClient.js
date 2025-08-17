ed8cd0cc00719051b9b8cfe2ccb85f29d4378b2e
cirellapietro <133475613+cirellapietro@users.noreply.github.com>
2025-08-17 16:41:58 +0200

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Inserisci qui i tuoi valori
+const supabaseUrl = "https://jamttxwhexlvbkjccrqm.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUz"
