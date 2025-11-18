b0a3864c23ee3116ebd6df04c53bca905e103267
cirellapietro <133475613+cirellapietro@users.noreply.github.com>
2025-11-18 08:17:53 +0100

    Create VehicleManager.tsx

---
+++ src/components/VehicleManager.tsx
@@ -0,0 +1,14 @@
+// src/components/VehicleManager.tsx
+import React from 'react';
+import './VehicleManager.css'; // Ora puoi mantenerlo
+
+const VehicleManager: React.FC = () => {
+  return (
+    <div className="vehicle-manager">
+      <h2>Gestione Veicoli</h2>
+      <p>Componente VehicleManager</p>
+    </div>
+  );
+};
+
+export default VehicleManager;
